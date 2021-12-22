ed4e703bc90c673075f495ec2567ee1cc978ea0a
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-12-22 14:50:41 +0100

    Hash password before saving it into the database

--- src/models/account.js
+++ src/models/account.js
@@ -1,7 +1,11 @@
+const bcrypt = require('bcrypt')
+
 const getCreateAccount = (pool) => {
     return async ({ username, email, password, firstname, lastname }) => {
+        const hashedPassword = bcrypt.hashSync(password, 10);
+
         const query = await pool.query(`INSERT INTO account (username, email, password, fname, lname) 
-        VALUES ($1,$2,$3,$4,$5) RETURNING id`, [username, email, password, firstname, lastname])
+        VALUES ($1,$2,$3,$4,$5) RETURNING id`, [username, email, hashedPassword, firstname, lastname])
 
         return query?.rows?.length > 0 ? query?.rows[0] : null
     }
