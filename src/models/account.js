4fbf15d884fbbc89ce90eddc4faeeb4440d6dde7
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-12-22 15:33:17 +0100

    Get account by its id

--- src/models/account.js
+++ src/models/account.js
@@ -20,10 +20,20 @@ const getAccountByUserOrEmail = (pool) => {
     }
 }
 
+const getAccountById = (pool) => {
+    return async (id) => {
+        const query = await pool.query(`SELECT id, username, fname as firstname, lname as lastname, email, password, created_date, updated_date, last_login 
+        FROM account WHERE id = $1`, [id])
+
+        return query?.rows?.length > 0 ? query?.rows[0] : null
+    }
+}
+
 const getAccountModels = (pool) => {
     return {
         createAccount: getCreateAccount(pool),
-        getAccountByUserOrEmail: getAccountByUserOrEmail(pool)
+        getAccountByUserOrEmail: getAccountByUserOrEmail(pool),
+        getAccountById: getAccountById(pool)
     }
 }
 
