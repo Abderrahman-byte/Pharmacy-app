784f1c75a1b6b390cea7b05bb602da8107cb195c
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-12-22 11:46:45 +0100

    Test database connection

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const app = require('./src/app')
+const dbPool = require('./db')
 
 const PORT = 8000;
 
-const startServer = () => {
-    const App = app()
+const testPoolConnection = async () => {
+    try {
+        const client = await dbPool.connect()
+        client.release()
+        console.log('[INFO] connected to database')
+        return true
+    } catch (err) {
+        console.error('[ERROR] couldn\'t connect to database')
+        console.error(err)
+        return false
+    }
+}
+
+
+const startServer = async () => {
+    if ( !(await testPoolConnection()) ) return
+
+    const App = app(dbPool)
 
     App.listen(PORT, (err) => {
         if (err) return console.error(`[ERROR] couldn\'t start the server on ${PORT}\n${err}`)
