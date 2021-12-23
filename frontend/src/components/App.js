2db8ba18b8fea417022696ae706766125f370b50
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-12-23 11:39:11 +0100

    add admin and auth page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router'
 
 import MainPage from '../pages/Main.page'
+import AdminPage from '../pages/Admin.page'
+import AuthPage from '../pages/Auth.page'
 
 import '../styles/App.css'
 
 function App() {
     return (
         <Routes>
+			<Route path="/admin/*" element={<AdminPage />} />
+			<Route path="/auth/*" element={<AuthPage />} />
             <Route path="/*" element={<MainPage />} />
         </Routes>
     )
---
+++ frontend/src/pages/Admin.page.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const AdminPage = () => {
+    return (
+        <div className="AdminPage">
+            <p>This an admin page<br/>Because you're an admin we let you in.</p>
+        </div>
+    )
+}
+
+export default AdminPage
