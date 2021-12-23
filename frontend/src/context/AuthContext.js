025a61269daf09fb41cefef3173df2447848aa56
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-12-23 12:21:17 +0100

    Fetch user data if logged in

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,10 +1,25 @@
 import React, { createContext, useEffect, useState } from 'react'
 
+import { getRequest } from '../utils/http'
+
 export const AuthContext = createContext({})
 
 export const AuthProvider = ({ children}) => {
     const [authenticated, setAuth] = useState(undefined)
     const [userData, setUserData] = useState(undefined)
+
+    useEffect(async () => {
+        const data = await getRequest('api/auth')
+
+        if (!data) {
+            setAuth(false)
+            setUserData(null)
+        } else {
+            setAuth(true)
+            setUserData(data)
+        }
+
+    }, [])
 
     return (
         <AuthContext.Provider value={{ authenticated, userData }} >
