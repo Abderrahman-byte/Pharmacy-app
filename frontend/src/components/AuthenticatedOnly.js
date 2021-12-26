33873098ad645fe19abba84f1267ef633a4e463c
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-12-26 13:08:59 +0100

    Fix delay bug

--- frontend/src/components/AuthenticatedOnly.js
+++ frontend/src/components/AuthenticatedOnly.js
@@ -1,21 +1,16 @@
 import React, { useContext } from "react"
 import { Navigate, useLocation } from 'react-router-dom'
+
 import { AuthContext } from "../context/AuthContext"
 
 const AuthenticatedOnly = ({ children }) => {
-    const { authenticated, userData } = useContext(AuthContext)
-    const { pathname } = useLocation()
+    const { authenticated } = useContext(AuthContext)
+    const location = useLocation()
 
-    if (authenticated === undefined) {
-        return (<></>)
-    } else if (!authenticated) {
-        return (<Navigate to={{
-            pathname: "/auth/login",
-            state: { from: pathname }
-        }} /> )
-    } else {
-        return (<>{children}</>)
-    }
+    if (authenticated === undefined) return (<></>)
+    else if (authenticated) return (<>{children}</>) 
+
+    return (<Navigate to="/auth/signup" state={{ from: location?.pathname }} />)
 }
 
 export default AuthenticatedOnly
