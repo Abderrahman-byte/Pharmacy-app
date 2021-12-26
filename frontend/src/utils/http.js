eca93b9cdeed02bdf98d7d2f032b33a8d26064b5
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-12-26 13:13:31 +0100

    Do post request

--- frontend/src/utils/http.js
+++ frontend/src/utils/http.js
@@ -2,11 +2,28 @@ const apiHost = 'http://localhost:8000'
 
 export const getRequest = async (path) => {
     try {
-        const response = await fetch(`${apiHost}/${path}`)
-        const data = response.json()
+        const response = await fetch(`${apiHost}/${path}`, {
+            credentials: 'include',
+        })
+        const data = await response.json()
 
-        if (data.ok && data.data) return data.data
-        else if (data.ok) return data
+        return data
+    } catch {}
+
+    return null
+}
+
+export const postRequest = async (path, data) => {
+    try {
+        const response = await fetch(`${apiHost}/${path}`, {
+            body: data,
+            method: 'POST',
+            credentials: 'include',
+            headers: { 'Content-Type': 'application/json' },
+        })
+        const returnedData = await response.json()
+
+        return returnedData
     } catch {}
 
     return null
