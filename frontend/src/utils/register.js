0bd35b42157459daac95ca3825a80703ad6c5b6f
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-12-26 13:11:45 +0100

    Represent register fields into a array

---
+++ frontend/src/utils/register.js
@@ -0,0 +1,38 @@
+export const fields = [
+    {
+        name: 'username',
+        type: 'text',
+        displayName: 'Username',
+        isRequired: true,
+    },
+    {
+        name: 'firstname',
+        type: 'text',
+        displayName: 'Firstname',
+        isRequired: true,
+    },
+    {
+        name: 'lastname',
+        type: 'text',
+        displayName: 'Lastname',
+        isRequired: true,
+    },
+    {
+        name: 'email',
+        type: 'email',
+        displayName: 'Email Address',
+        isRequired: true,
+    },
+    {
+        name: 'password',
+        type: 'password',
+        displayName: 'Password',
+        isRequired: true,
+    },
+    {
+        name: 'password2',
+        type: 'password',
+        displayName: 'Password Confirmation',
+        isRequired: true,
+    },
+]
