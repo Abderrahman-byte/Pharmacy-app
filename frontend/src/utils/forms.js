2fea569a1ddc77c2b74b3be80d875178f3b76193
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-12-27 10:21:19 +0100

    Add login fields

---
+++ frontend/src/utils/forms.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+export const getfieldsComponents = (fields, onChangeCallback) => {
+    return fields.map((fieldData) => <div key={fieldData.name} className='form-div'>
+        <label htmlFor={fieldData.name + '-input'}>{fieldData.displayName}</label>
+        <input onChange={onChangeCallback} type={fieldData.type} name={fieldData.name} id={fieldData.name + '-input'} required={fieldData.isRequired} />
+    </div>)
+}
+
+export const registerFields = [
+    {
+        name: 'username',
+        type: 'text',
+        displayName: 'Username',
+        isRequired: true,
+    },
+    {
+        name: 'firstname',
+        type: 'text',
+        displayName: 'Firstname',
+        isRequired: true,
+    },
+    {
+        name: 'lastname',
+        type: 'text',
+        displayName: 'Lastname',
+        isRequired: true,
+    },
+    {
+        name: 'email',
+        type: 'email',
+        displayName: 'Email Address',
+        isRequired: true,
+    },
+    {
+        name: 'password',
+        type: 'password',
+        displayName: 'Password',
+        isRequired: true,
+    },
+    {
+        name: 'password2',
+        type: 'password',
+        displayName: 'Password Confirmation',
+        isRequired: true,
+    },
+]
+
+
+export const loginFields = [
+    {
+        name: 'username',
+        type: 'text',
+        displayName: 'Username',
+        isRequired: true,
+    },
+    {
+        name: 'password',
+        type: 'password',
+        displayName: 'Password',
+        isRequired: true,
+    }
+]
