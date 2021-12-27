3d0a47a070229142cb4495c853894f8a54c55b74
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-12-27 11:38:03 +0100

    Finish Register page

--- frontend/src/pages/Register.page.js
+++ frontend/src/pages/Register.page.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 import RegisterForm from '../components/RegisterForm'
 
 import '../styles/RegisterPage.scss'
 
 const RegisterPage = () => {
+    const location = useLocation()
+    
     return (
         <div className='RegisterPage'>
-            <h1>Sign up</h1>
-            <RegisterForm />
+            <div className='form-card'>
+                <h1 className='form-title'>Sign up</h1>
+                <hr />
+                <RegisterForm />
+                <hr />
+                <div className='info-div'>
+                    <p>Already have an account?</p>
+                    <Link to='/auth/login' state={{from : location?.state?.from || '/'}} >Login</Link>      
+                </div>
+            </div>
         </div>
     )
 }
