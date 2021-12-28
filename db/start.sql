136c113e453ba05a0b158fc817c7593774df02a9
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-12-28 11:25:27 +0100

    Products tables

--- db/start.sql
+++ db/start.sql
@@ -1,4 +1,7 @@
 DROP TABLE IF EXISTS account;
+DROP TABLE IF EXISTS product_image;
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS product_inventory;
 
 CREATE TABLE account (
     id SERIAL PRIMARY KEY,
@@ -6,8 +9,34 @@ CREATE TABLE account (
     fname VARCHAR (200) NOT NULL,
     lname VARCHAR (200) NOT NULL,
     email VARCHAR (200) NOT NULL UNIQUE,
+    is_admin Boolean NOT NULL DEFAULT false,
     password TEXT NOT NULL,
     created_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     updated_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     last_login TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
-)
+);
+
+CREATE TABLE product_inventory (
+    id SERIAL PRIMARY KEY,
+    quantity INT NOT NULL DEFAULT 0,
+    created_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE product (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR (200) NOT NULL,
+    price NUMERIC (9,6) NOT NULL,
+    description TEXT,
+    inventory_id INT REFERENCES product_inventory (id),
+    created_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+
+    CHECK (price > 0)
+);
+
+CREATE TABLE product_image (
+    id SERIAL PRIMARY KEY,
+    url TEXT NOT NULL,
+    product_id INT NOT NULL REFERENCES product (id) ON DELETE CASCADE
+);
