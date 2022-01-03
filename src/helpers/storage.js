9809b3d49a1481160a236e793c05191e00e1a602
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-01-03 21:29:58 +0100

    Destroy image from cdn

--- src/helpers/storage.js
+++ src/helpers/storage.js
@@ -34,6 +34,32 @@ const uploadFile = async (path) => {
     return response.data
 }
 
+const destroyFile = async (id) => {
+    const formData = new FormData()
+
+    const ts = Math.floor(Date.now() / 1000)
+    const data = { timestamp: ts, public_id: id }
+    const queryString = Object.keys(data).sort().map(key => {
+        return encodeURIComponent(key) + '=' + data[key]
+    }).join('&')
+
+    formData.append('public_id', id)
+    formData.append('timestamp', ts)
+    formData.append('api_key', apiKey)
+    formData.append('signature', SHA1(queryString + secret))
+
+    const response = await axios.post(`https://api.cloudinary.com/v1_1/${cloudname}/image/destroy`,formData, {
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Basic ${basicAuth(apiKey, secret)}`,
+            ...formData.getHeaders()
+        },
+    })
+
+    return response.data
+}
+
 module.exports = {
-    uploadFile
+    uploadFile,
+    destroyFile
 }
